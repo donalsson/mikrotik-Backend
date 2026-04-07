2669757503336ea4643b5b7c3d1dcbb90a25a8eb
donalsson <franky45kouekem@gmail.com>
2026-04-06 21:03:34 -0400

    Change default PORT from 3000 to 8080

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 async function getConnection() {
   const device = new MikroNode({
